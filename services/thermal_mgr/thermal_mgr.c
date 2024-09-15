73b031b1baf227cafff25cb911b6fda0becfcb41
Justin Bax <justin.bax@icloud.com>
2024-09-15 11:42:57 -0400

    finished task and os handler

--- services/thermal_mgr/thermal_mgr.c
+++ services/thermal_mgr/thermal_mgr.c
@@ -2,6 +2,7 @@
 #include "errors.h"
 #include "lm75bd.h"
 #include "console.h"
+#include "logging.h"
 
 #include <FreeRTOS.h>
 #include <os_task.h>
@@ -44,17 +45,57 @@ void initThermalSystemManager(lm75bd_config_t *config) {
 error_code_t thermalMgrSendEvent(thermal_mgr_event_t *event) {
   /* Send an event to the thermal manager queue */
 
+  if (event == NULL) {
+    return ERR_CODE_INVALID_ARG;
+  }
+
+  if (!xQueueSend(thermalMgrQueueHandle, event, 10)) {
+    return ERR_CODE_QUEUE_FULL;
+  }
+
   return ERR_CODE_SUCCESS;
 }
 
 void osHandlerLM75BD(void) {
-  /* Implement this function */
+  thermal_mgr_event_t event;
+  event.type = THERMAL_MGR_EVENT_OS_INTERRUPT;
+  thermalMgrSendEvent(&event);
 }
 
 static void thermalMgr(void *pvParameters) {
-  /* Implement this task */
   while (1) {
-    
+    thermal_mgr_event_t event;
+    BaseType_t eventReceived = xQueueReceive(thermalMgrQueueHandle, &event, 10);
+
+    if (eventReceived == pdTRUE) {
+      // Switch case to easily allow for more events in the future
+      switch (event.type) {
+        case THERMAL_MGR_EVENT_MEASURE_TEMP_CMD:
+        case THERMAL_MGR_EVENT_OS_INTERRUPT:
+          // Both cases are practically the same, except we do more logging at the end if it's OS_INTERRUPT
+          if (pvParameters == NULL) continue;
+          lm75bd_config_t config = *(lm75bd_config_t *)(pvParameters);
+
+          float temp = 0.0f;
+          if (readTempLM75BD(config.devAddr, &temp) == ERR_CODE_SUCCESS) {
+            addTemperatureTelemetry(temp);
+          }
+
+          if (event.type == THERMAL_MGR_EVENT_OS_INTERRUPT) {
+            if (temp >= config.overTempThresholdCelsius) {
+              overTemperatureDetected();
+            } else if (temp <= config.hysteresisThresholdCelsius) {
+              safeOperatingConditions();
+            } else {
+              LOG_ERROR_CODE(ERR_CODE_INVALID_STATE);
+            }
+          }
+          break;
+        default:
+          LOG_ERROR_CODE(ERR_CODE_INVALID_STATE);
+          break;
+      }
+    }
   }
 }
 
